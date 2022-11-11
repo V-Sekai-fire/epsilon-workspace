014252d76db3d9246c62cfb15ea52415d5420920
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-11 09:04:16 -0500

    Cleanup.

--- demo/ewbik/core/bone_constrain.gd
+++ demo/ewbik/core/bone_constrain.gd
@@ -132,7 +132,9 @@ func _run():
 		ewbik.set_constraint_name(constraint_i, bone_name)
 		ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-360),  deg_to_rad(360)))
 #		# https://pubmed.ncbi.nlm.nih.gov/32644411/
-		if bone_name in ["RightLowerLeg"]:
+		if bone_name.is_empty() in []:
+			pass
+		elif bone_name in ["RightLowerLeg"]:
 			ewbik.set_kusudama_limit_cone_count(constraint_i, 3)
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 1, 0))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(5))
