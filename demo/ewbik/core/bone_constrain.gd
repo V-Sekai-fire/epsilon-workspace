2dff78c28cb85389c7a736b2286ef91ea4500a2b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-11 10:24:18 -0800

    Fix axial limits using visualizer.

--- demo/ewbik/core/bone_constrain.gd
+++ demo/ewbik/core/bone_constrain.gd
@@ -229,14 +229,14 @@ func _run():
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(5))
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 1, Vector3(-1, -1, 0))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 1, deg_to_rad(20))
-			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-130),  deg_to_rad(130)))
+			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-30),  deg_to_rad(150)))
 		elif bone_name in ["LeftLowerLeg"]:
 			ewbik.set_kusudama_limit_cone_count(constraint_i, 3)
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 1, 0))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(5))
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 1, Vector3(-1, -1, 0))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 1, deg_to_rad(20))
-			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-130),  deg_to_rad(130)))
+			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-30),  deg_to_rad(150)))
 		elif bone_name in ["RightFoot"]:
 			ewbik.set_kusudama_limit_cone_count(constraint_i, 2)
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 1, 0))
