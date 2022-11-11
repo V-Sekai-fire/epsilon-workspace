4bdfe4c025993100dc87b566a0b097bca4aab99f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-10 17:29:15 -0800

    Add range of motions limits.

--- demo/ewbik/core/bone_constrain.gd
+++ demo/ewbik/core/bone_constrain.gd
@@ -142,22 +142,14 @@ func _run():
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(5))
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 1, Vector3(1, 0, -1))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 1, deg_to_rad(5))
+			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(90), deg_to_rad(-90)))
 		elif bone_name in ["LeftLowerLeg"]:
 			ewbik.set_kusudama_limit_cone_count(constraint_i, 2)
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 1, 0))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(5))
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 1, Vector3(1, 0, -1))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 1, deg_to_rad(5))
-#		elif bone_name in ["RightUpperLeg"]:
-#			ewbik.set_kusudama_limit_cone_count(constraint_i, 1)
-#			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, -1, 0))
-#			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(45))
-##			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-30), deg_to_rad(30)))
-#		elif bone_name in ["LeftUpperLeg"]:
-#			ewbik.set_kusudama_limit_cone_count(constraint_i, 1)
-#			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, -1, 0))
-#			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(45))
-##			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-30), deg_to_rad(30)))
+			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(90), deg_to_rad(-90)))
 		elif bone_name in ["Chest", "UpperChest"]:
 			ewbik.set_kusudama_limit_cone_count(constraint_i, 1)
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 1, 0))
